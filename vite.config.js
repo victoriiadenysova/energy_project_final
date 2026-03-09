19aafb601b5a25408eb2d35f3a11eb7143d8fa68
unknown <Yan>
2026-03-09 20:34:18 +0200

    changed

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base : '/energy_project_3/',
+    base : '/energy_project_final/',
     envDir: '../',
     headers: [
       {
